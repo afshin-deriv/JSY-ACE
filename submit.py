cce755080a8316aac333af5537125c767cc918bf
Chao <42952910+kenwan006@users.noreply.github.com>
2021-02-17 23:33:27 -0800

    Update submit.py

--- submit.py
+++ submit.py
@@ -3,7 +3,7 @@ import sys
 import swpag_client
 
 print("Welcome!")
-t = swpag_client.Team('http://52.52.83.248', 'pc9Zhk3DDDV9Y5ILrUs2')
+t = swpag_client.Team('http://13.56.188.54', 'pc9Zhk3DDDV9Y5ILrUs2')
 flag = input("Input flag: ")
 print(t.submit_flag(['%s']), flag)
 # print("Submit flag: %s", str(sys.argv[1]))
